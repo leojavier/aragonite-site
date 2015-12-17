c7f2a55577f4cfb34d465e0afd1c20a2d9b061b1
leojavier <info@leojavier.com>
2015-12-17 10:00:47 -0500

    Updating version, adding google analytics

--- js/aragonite.js
+++ js/aragonite.js
@@ -1,33 +1,37 @@
+/*jslint node: true */
 var aragonite = (function () {
     //'use strict';
 
     var me = this;
     var form = null;
-    var elements = null;
 
-    Regex = {
+    var Regex = {
         // List of regex to validate fields
         alphaNumeric: /^[A-Z,a-z,0-9, -_]+$/,
         string: /^[A-Z,a-z ,.'-]+$/,
         email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         phone: /^[0-9]{10,15}/,
         required: '',
-        username: /^([a-zA-Z0-9_-]){6,20}/,
-        password: /^([a-zA-Z0-9_!@#$%^&*-]){8,12}/
+        visa: /^4[0-9]{12}(?:[0-9]{3})?$/,//All Visa card numbers start with a 4. New cards have 16 digits. Old cards have 13.
+        masterCard: /^5[1-5][0-9]{14}$/,//All MasterCard numbers start with the numbers 51 through 55. All have 16 digits.
+        amex: /^3[47][0-9]{13}$/,//American Express card numbers start with 34 or 37 and have 15 digits.
+        discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/,//Discover card numbers begin with 6011 or 65. All have 16 digits.
+        cvv: /^[0-9]{3,4}$///Credit cards security code. from 3 to 4 digits
     };
 
-    controller = {
+    var controller = {
         validate: function (opt) {
+            var reg = new RegExp('(\\s|^)required(\\s|$)');
 
             if (!opt.value.match(Regex[opt.dataType])) {
                 opt.field.setAttribute('class', 'required');
             } else {
-                function hasClass(ele, cls) {
+                var hasClass = function (ele, cls) {
                     return ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
-                }
+                };
 
                 if (hasClass(opt.field, 'required')) {
-                    var reg = new RegExp('(\\s|^)required(\\s|$)');
+
                     opt.field.className = opt.field.className.replace(reg, ' ');
                 }
             }
@@ -40,19 +44,20 @@ var aragonite = (function () {
         },
         submit: function (e) {
 
-            var elements = me.elements;
+            var group,
+                elements;
 
             if (elements) {
-                var group = elements.length;
+                group = elements.length;
             } else {
                 this.elements = document.getElementsByTagName('input');
-                var elements = this.elements;
-                var group = elements.length;
+                elements = this.elements;
+                group = elements.length;
             }
 
             for (var i = 0; i < group; i++) {
 
-                me.controller.validate({
+                controller.validate({
                     field: elements[i],
                     value: elements[i].value,
                     dataType: elements[i].getAttribute('data-type'),
@@ -67,14 +72,78 @@ var aragonite = (function () {
             else {
                 e.submit(this.form);
             }
+        },
+
+        /**
+         * Add custom regular expression to the Regex object
+         * @param {obj} [option] Takes an object with two properties. The name of the field and the regular expression to use for validation
+         * returns void
+         */
+        getRegex: function (opt) {
+            var options = opt,
+                optionsQty = options.length;
+
+            for (var i = 0; i < optionsQty; i++) {
+                var item = options[i].regex.toString(),
+                    firstChar = item.substr(0, 1),
+                    lastChar = item.substr(item.length - 1, item.length);
+
+                if (typeof (options[i].regex) === "object" && firstChar === "/" && lastChar === "/") {
+                    Regex[options[i].name] = options[i].regex;
+                }
+            }
+        },
+
+        /**
+         * Will atach an event handler to an element
+         * @param {HTML element} [element] takes the element and assign an event handler
+         * returns void
+         */
+        bind: function (element, event) {
+            if (element.addEventListener) {
+                element.addEventListener(event, function (e) {
+                    var target;
+                    if (e.target) {
+                        target = e.target;
+                    } else if (e.srcElement) {
+                        target = e.srcElement;
+                    }
+                    controller.validate({
+                        field: target,
+                        value: target.value,
+                        dataType: target.getAttribute('data-type'),
+                        optional: target.getAttribute('data-optional')
+                    });
+                });
+            } else if (element.attachEvent) {
+                element.attachEvent(event, function (e) {
+                    var target;
+                    if (e.target) {
+                        target = e.target;
+                    } else if (e.srcElement) {
+                        target = e.srcElement;
+                    }
+                    controller.validate({
+                        field: target,
+                        value: target.value,
+                        dataType: target.getAttribute('data-type'),
+                        optional: target.getAttribute('data-optional')
+                    });
+                });
+            }
         }
     };
+
     /**
      * Initiate the validator
      * @param {string} [elementId] Takes the ID of the form without #
      * returns void
      */
-    init = function (formId) {
+    var init = function (formId, opt) {
+
+        if (opt.regex) {
+            controller.getRegex(opt.regex);
+        }
 
         form = document.getElementById(formId);
 
@@ -82,48 +151,17 @@ var aragonite = (function () {
         var searchFields = form.children;
 
         for (var i = 0; i < searchFields.length; i++) {
-                fields.push(searchFields[i]);
+            fields.push(searchFields[i]);
         }
 
         var group = fields.length;
 
-
         var validate = function () {
             for (var i = 0; i < group; i++) {
-                var events = ['keyup', 'blur']
+                var events = ['keyup', 'blur'];
 
                 for (var e = 0; e < events.length; e++) {
-                    if (fields[i].addEventListener) {
-                        fields[i].addEventListener(events[e], function (e) {
-                            var target;
-                            if (e.target) {
-                                target = e.target;
-                            } else if (e.srcElement) {
-                                target = e.srcElement;
-                            }
-                            me.controller.validate({
-                                field: target,
-                                value: target.value,
-                                dataType: target.getAttribute('data-type'),
-                                optional: target.getAttribute('data-optional')
-                            });
-                        });
-                    } else if (fields[i].attachEvent) {
-                        fields[i].attachEvent(events[e], function (e) {
-                            var target;
-                            if (e.target) {
-                                target = e.target;
-                            } else if (e.srcElement) {
-                                target = e.srcElement;
-                            }
-                            me.controller.validate({
-                                field: target,
-                                value: target.value,
-                                dataType: target.getAttribute('data-type'),
-                                optional: target.getAttribute('data-optional')
-                            });
-                        });
-                    }
+                    controller.bind(fields[i], events[e]);
                 }
 
             }
@@ -136,7 +174,7 @@ var aragonite = (function () {
                     return false;
                 }
                 else if (e.which === 13) {
-                    me.controller.submit(form);
+                    controller.submit(form);
                 } else if (e.which === 9) {
                     validate();
                 }
@@ -144,7 +182,7 @@ var aragonite = (function () {
 
             form.addEventListener('submit', function (e) {
                 e.preventDefault();
-                me.controller.submit(form);
+                controller.submit(form);
 
             });
 
@@ -154,7 +192,7 @@ var aragonite = (function () {
                     return false;
                 }
                 else if (e.which === 13) {
-                    me.controller.submit(form);
+                    controller.submit(form);
                 } else if (e.which === 9) {
                     validate();
                 }
@@ -162,17 +200,26 @@ var aragonite = (function () {
 
             form.attachEvent('submit', function (e) {
                 e.preventDefault();
-                me.controller.submit(form);
+                controller.submit(form);
 
             });
         }
-
-        
-
-
     };
-
-    return {
-        init: init
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        return {
+            init: init,
+            regex: Regex
+        };
+    } else {
+        return {
+            init: init
+        };
     }
 })();
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = aragonite;
+}
+else {
+    window.aragonite = aragonite;
+}
